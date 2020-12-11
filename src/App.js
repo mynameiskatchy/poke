e9d3596573663113cb67d2e7b7366ff99640a162
mynameiskatchy <karchietrinh@gmail.com>
2020-12-10 22:36:08 -0500

    Cancel old request if there is new request

--- src/App.js
+++ src/App.js
@@ -11,12 +11,18 @@ function App() {
 
   useEffect(() => {
     setLoading(true)
-    axios.get(currentPageUrl).then(res => {
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
       setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.prev)
       setPokemon(res.data.results.map(p => p.name))
     })
+
+    return () => cancel()
+
   }, [currentPageUrl])
 
   if (loading) return "Loading..."
