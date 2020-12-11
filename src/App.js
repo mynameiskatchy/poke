cafedb5e440dd6e6423ecd035df5c74485155225
mynameiskatchy <karchietrinh@gmail.com>
2020-12-10 21:17:10 -0500

    fetch first page of results from pokemon api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  const [pokemon, setPokemon] = useState([])
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
   
   return (
     <PokemonList pokemon={pokemon} />
